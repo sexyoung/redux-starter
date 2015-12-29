45a804e3a929a990a36d9bd4ac83b23bf9462182
ben-hsieh <ben.hsieh@tarokosoftware.com>
2015-12-30 01:08:54 +0800

    最後把 todoApp.js 改成用 index.js 引入其他兩隻 reducer 取代 todoStore.js 再 import ../reducers 到 createStore 產生一個真的 Store 回去

--- store/todoStore.js
+++ store/todoStore.js
@@ -1,6 +1,6 @@
 import {createStore} from "redux";
-import todo from "../reducers/todoApp";
+import todoApp from '../reducers/';
 
 // 把 reducer 做成 store (未來也可能會有多個 reducer)
-let store = createStore(todo);
+let store = createStore(todoApp);
 export default store;
