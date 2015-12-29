dc63cef33e18a5f0d5e0e5f1cd54a08c10e138e9
ben-hsieh <ben.hsieh@tarokosoftware.com>
2015-12-29 18:19:33 +0800

    把 reducer 在檔案裡切割成兩個 function

--- reducers/todoApp.js
+++ reducers/todoApp.js
@@ -12,28 +12,8 @@ const initialState = {
   todos: []
 }
 
-/**
- * 定義 reducer
- * 它好像是 store 的一部分,
- * 只有 reducer 會改變整個 store 的內容
- * 另外 default 的意思就是專指回傳這個
- * 有人 import a from "./counter.js"
- * a 指的就是 counter()
- *
- * 但如果沒有寫 default
- * 那就要寫 import {counter} from "./counter.js"
- */
-export default function todoApp(state = initialState, action) {
-  switch (action.type) {
-    case SET_VISIBILITY_FILTER:
-      /**
-       * Object.assign() 是 ES6 的一部分
-       * 不過大部份瀏覽器尚未實作。
-       * 你會需要使用一個 polyfill
-       */
-      return Object.assign({}, state, {
-        visibilityFilter: action.filter
-      });
+function todos(state, action){
+  switch(action.type){
     case ADD_TODO:
       return Object.assign({}, state, {
         /**
@@ -61,6 +41,38 @@ export default function todoApp(state = initialState, action) {
           ...state.todos.slice(action.index + 1)
         ]
       });
+    default:
+      return state;
+  }
+}
+
+/**
+ * 定義 reducer
+ * 它好像是 store 的一部分,
+ * 只有 reducer 會改變整個 store 的內容
+ * 另外 default 的意思就是專指回傳這個
+ * 有人 import a from "./counter.js"
+ * a 指的就是 counter()
+ *
+ * 但如果沒有寫 default
+ * 那就要寫 import {counter} from "./counter.js"
+ */
+export default function todoApp(state = initialState, action) {
+  switch (action.type) {
+    case SET_VISIBILITY_FILTER:
+      /**
+       * Object.assign() 是 ES6 的一部分
+       * 不過大部份瀏覽器尚未實作。
+       * 你會需要使用一個 polyfill
+       */
+      return Object.assign({}, state, {
+        visibilityFilter: action.filter
+      });
+    case ADD_TODO:
+    case COMPLETE_TODO:
+      return Object.assign({}, state, {
+        todos: todos(state.todos, action);
+      })
 
     default:
       return state;
