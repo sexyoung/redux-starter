5db1468ac612ffd73323ad610a83368d2ddd514f
ben-hsieh <ben.hsieh@tarokosoftware.com>
2015-12-30 15:29:30 +0800

    只要 store 有變化就 console 這好屌

--- index.js
+++ index.js
@@ -6,22 +6,22 @@ import {addTodo, setVisibilityFilter, completeTodo} from './actions/todo';
 // 透過 getState() 取得目前的 state
 console.log(store.getState());
 
-// 透過 dispatch() 發送我們剛剛所定義的 action
-/**
- * 通常是這樣叫 store.dispatch({type: XXX, value})
- */
+// 每次 store 有更動就 console 它
+let unsubscribe = store.subscribe(() =>
+  console.log(store.getState())
+)
+
+
 store.dispatch(addTodo("你好啊"));
 
-// 看執行後的 state
-console.log(store.getState());
-
+// 新增項目
 store.dispatch(addTodo());
-console.log(store.getState());
 
 // 顯示欄為 秀 已完成
 store.dispatch(setVisibilityFilter('SHOW_COMPLETED'));
-console.warn(store.getState());
 
 // 完成 index 1
 store.dispatch(completeTodo(1));
-console.warn(store.getState());
+
+unsubscribe();
+store.dispatch(completeTodo(0));
